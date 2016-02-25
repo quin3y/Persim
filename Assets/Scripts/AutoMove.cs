3416f0323d639e3470b2d79cfb3b08a840ef45a7
Quincy <longlongquincy@gmail.com>
2016-02-24 21:25:47 -0500

    Added contact sensor

---
+++ Assets/Scripts/AutoMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMove : MonoBehaviour {
+	int direction = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(0.02f * direction, 0f, 0f));
+
+		if (transform.position.x > -13.5 || transform.position.x < -14)    direction *= -1;
+	}
+}
---
+++ Assets/Scripts/ContactSensor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContactSensor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Door frame") {
+			print("door closed");
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.gameObject.tag == "Door frame") {
+			print("door opened");
+		}
+	}
+}
