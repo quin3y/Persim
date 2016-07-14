af2cdc0b048ff5d484c1523c10e0bea5e5781d33
JW <jwlee@ucmo.edu>
2016-07-14 13:59:46 -0400

    Loop starts

---
+++ Assets/Scripts/ContextGraph.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+	public class ContextGraph
+	{
+		public ContextGraph ()
+		{
+			
+		}
+	}
+}
+
